1c5c602b7440c6cdf9d107b0c080d7590f7df352
zouter <wouter.saelens@gmail.com>
2026-03-16 12:58:31 +0000

    Deploying to gh-pages from @ zouter/zouter.github.io@516a4c75316ae2dbec09880d5304fcb2a2f28e41 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -33,8 +33,8 @@ ninja.data = [{
         },{id: "news-we-started-our-lab",
           title: 'We started our lab!',
           description: "",
-          section: "News",},{id: "news-we-will-soon-join-vib-ai-as-a-core-member",
-          title: 'We will soon join VIB.AI as a core member.',
+          section: "News",},{id: "news-we-have-joined-the-vib-ai-as-a-core-member",
+          title: 'We have joined the VIB.AI as a core member.',
           description: "",
           section: "News",},{
         id: 'social-email',
